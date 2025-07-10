88f44ff7c51b060dac6f253fb339cad22f135350
Unitycorn <flaig.m@gmx.de>
2025-07-10 16:02:08 +0200

    Docstrings, docstrings everywhere

--- products.py
+++ products.py
@@ -1,50 +1,53 @@
 class Product:
+    """Represents a product"""
     def __init__(self, name: str, price: float, quantity: int):
         if name == "" or not isinstance(name, str):
             raise ValueError(f"Inappropriate product name: {name}")
-        else:
-            self.name = name
+        self.name = name
         if price < 0:
             raise ValueError(f"Inappropriate price value: {price}")
-        else:
-            self.price = price
+        self.price = price
         if quantity < 0 or not isinstance(quantity, int):
             raise ValueError(f"Inappropriate quantity value: {quantity}")
-        else:
-            self.quantity = quantity
+        self.quantity = quantity
         if self.quantity <= 0:
             self.active = False
         else:
             self.active = True
 
     def get_quantity(self) -> int:
+        """Getter for quantity"""
         return self.quantity
 
     def set_quantity(self, quantity):
+        """Setter for quantity"""
         self.quantity += quantity
         if self.quantity <= 0:
             self.deactivate()
 
     def is_active(self) -> bool:
+        """Check if item is available"""
         return self.active
 
     def activate(self):
+        """Activates an item"""
         self.active = True
 
     def deactivate(self):
+        """Deactivates an item"""
         self.active = False
 
     def show(self):
+        """Return string representation if item is available"""
         if self.active:
             return f"{self.name}, Price: ${self.price}, Quantity: {self.quantity}"
-        else:
-            return None
+        return None
 
     def buy(self, quantity) -> float:
+        """Decreases quantity and returns the total price"""
         if self.quantity - quantity < 0:
             raise EnvironmentError("Quantity outcome is negative")
-        else:
-            if self.quantity - quantity == 0:
-                self.deactivate()
-            self.quantity -= quantity
+        if self.quantity - quantity == 0:
+            self.deactivate()
+        self.quantity -= quantity
         return float(self.price * quantity)
--- store.py
+++ store.py
@@ -2,6 +2,7 @@ import products
 
 
 class Store:
+    """Represents a store"""
     def __init__(self, product_list):
         if product_list is None:
             self.product_list = []
@@ -9,21 +10,26 @@ class Store:
             self.product_list = product_list
 
     def add_product(self, product):
+        """Adds a product to the store"""
         self.product_list.append(product)
 
     def remove_product(self, product):
+        """Removes a product from the store"""
         self.product_list.remove(product)
 
     def get_total_quantity(self) -> int:
+        """Total quantity of items currently in the store"""
         total_quantity = 0
         for product in self.product_list:
             total_quantity += product.quantity
         return total_quantity
 
     def get_all_products(self) -> list[products.Product]:
+        """Returns all active products in the store"""
         return [product for product in self.product_list if product.is_active()]
 
     def order(self, shopping_list) -> float:
+        """Buys each item on the shopping list"""
         price = 0
         for product, quantity in shopping_list:
             price += product.buy(quantity)
